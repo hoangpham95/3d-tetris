425c4022ca7bf018d78fcf323db0684399a17c32
zhongsh <zhong.sh@husky.neu.edu>
2018-11-23 00:21:21 -0500

    Remove unneeded files

--- src/AllCubesSceneNode.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  AllCubesSceneNode.cpp
-//  3d-tetris
-//
-//  Created by Shanghao Zhong on 11/20/18.
-//  Copyright © 2018 Shanghao Zhong. All rights reserved.
-//
-
-#include "AllCubesSceneNode.hpp"
