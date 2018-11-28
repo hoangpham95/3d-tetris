2db44d357c45d633e0988617d1a260b5bc413a54
zhongsh <zhong.sh@husky.neu.edu>
2018-11-28 15:36:29 -0500

    Test CubeUnitFactory

--- src/main.cpp
+++ src/main.cpp
@@ -51,16 +51,43 @@ int main(int argc, const char * argv[]) {
     cubes[4]->RotateCubeUnitOverY();
     ExportLocationAsCSV(myCSV, &cubes[0], cubes.size());
     
+    cubes[0]->DeleteCubeUnit();
+    
+    CubeUnitFactory& factory = CubeUnitFactory::instance();
+    
     string description = "3 2 2 1 0 1 1 1 1 0 1 0 0 1 1";
-    Cube *cubeUnit = CubeUnitFactory::instance().ParseCubeUnit(description, 1);
+    Cube *cubeUnit = factory.ParseCubeUnit(description, 1);
+    cubeUnit->SetLocation(5, -5, 5);
+    cubeUnit->UpdateCubeUnitLocation();
     
     Cube *dupCubeUnit = cubeUnit->DuplicateCubeUnit();
-    dupCubeUnit->SetLocation(5, 5, 5);
+    dupCubeUnit->SetLocation(7, -7, 7);
     dupCubeUnit->RotateCubeUnitOverX(false);
     
     cubeUnit->ExportCubeUnitLocationAsCSV(myCSV);
     dupCubeUnit->ExportCubeUnitLocationAsCSV(myCSV);
     
+    cubeUnit->DeleteCubeUnit();
+    dupCubeUnit->DeleteCubeUnit();
+    
+    factory.AddNewCubeUnits("./other/cubes.txt");
+    Cube *tmp;
+//    for(int i = 0; i < 4; ++i) {
+//        tmp = factory.MakeCubeUnit(i, i);
+//        tmp->SetLocation(-4 + 2 * i, -4 + 2 * i, 0);
+//        tmp->UpdateCubeUnitLocation();
+//        tmp->ExportCubeUnitLocationAsCSV(myCSV);
+//        tmp->DeleteCubeUnit();
+//    }
+    
+    for(int i = 0; i < 10; ++i) {
+        tmp = factory.NewRandomCubeUnit();
+        tmp->SetLocation(-10 + 2 * i, -10 + 2 * i, 0);
+        tmp->UpdateCubeUnitLocation();
+        tmp->ExportCubeUnitLocationAsCSV(myCSV);
+        cout << tmp->GetID() << ' ';
+        tmp->DeleteCubeUnit();
+    }
     
     myCSV.close();
     
